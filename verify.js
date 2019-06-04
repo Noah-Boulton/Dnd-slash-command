ca930bb79d3fd664f93c5cf8477e528855328de6
Noah Boulton <noah.boulton@gmail.com>
2019-06-04 10:49:17 -0700

    Working on verifying requests

--- verify.js
+++ verify.js
@@ -8,7 +8,7 @@ function verify(request){
     const hmac = crypto.createHmac('sha256', process.env.SIGNING_SECRET);
     my_signature = 'v0=' + hmac.update(sig_basestring).digest('hex');
     slack_signature = request.headers['X-Slack-Signature'];
-    if (crypto.timingSafeEqual(my_signature, slack_signature)){
+    if (crypto.timingSafeEqual(Buffer.from(my_signature, 'utf8'), Buffer.from(slack_signature, 'utf8'))){
         console.log('safe');
     }
 }
