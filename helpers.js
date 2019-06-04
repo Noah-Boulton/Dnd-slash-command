00ca651da33271da91b21d63f26894c56fc79a25
Noah Boulton <noah.boulton@gmail.com>
2019-06-04 11:43:49 -0700

    Finished verify

---
+++ helpers.js
@@ -0,0 +1,33 @@
+const crypto = require('crypto');
+const qs = require('qs');
+
+function verify(request, res) {
+    const slack_signature = request.headers['x-slack-signature'];
+    const timestamp = request.headers['x-slack-request-timestamp'];
+    const request_body = qs.stringify(request.body, { format: 'RFC1738' });
+    const sig_basestring = 'v0:' + timestamp + ':' + request_body;
+
+    const my_signature = 'v0=' +
+        crypto.createHmac('sha256', 'b4c265244321b7decd02cd76398abcf3')
+            .update(sig_basestring, 'utf8')
+            .digest('hex');
+
+    return new Promise(function (resolve, reject) {
+        if (crypto.timingSafeEqual(Buffer.from(my_signature, 'utf8'), Buffer.from(slack_signature, 'utf8'))) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    });
+}
+
+function convertText(text) {
+    var terms = text.split(" ");
+    for (var i = 0; i < terms.length; i++) {
+        terms[i] = terms[i].charAt(0).toUpperCase() + terms[i].substring(1);
+    }
+    return terms;
+}
+
+exports.verify = verify;
+exports.convertText = convertText;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const verify = require('./verify');
+const helpers = require('./helpers');
 const bodyParser = require('body-parser');
 
 /*
@@ -12,162 +12,161 @@ const bodyParser = require('body-parser');
 
 const rawBodyBuffer = (req, res, buf, encoding) => {
     if (buf && buf.length) {
-      req.rawBody = buf.toString(encoding || 'utf8');
+        req.rawBody = buf.toString(encoding || 'utf8');
     }
-  };
-  
-app.use(bodyParser.urlencoded({verify: rawBodyBuffer, extended: true }));
+};
+
+app.use(bodyParser.urlencoded({ verify: rawBodyBuffer, extended: true }));
 app.use(bodyParser.json({ verify: rawBodyBuffer }));
 
-app.set('port', (process.env.PORT || 9001));
-
-//For local testing
-// app.set('port', (3000));
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), () => {
     console.log('Server listening on port %s', app.get('port'))
 });
 
 app.post('/cast', function (req, res) {
-    verify.verify(req);
-    const { command, text, response_url } = req.body;
-    if(text.toLowerCase() === 'help'){
-        res.send({
-            "response_type": "ephemeral",
-            "text": `Use \`/cast\` to find out about 5th Edition Dungeons and Dragons spells. Some examples include:`,
-            "attachments": [
-                {
-                    "text":"• \`/cast fireball\`\n• \`/cast acid arrow\`\n• \`/cast animate dead\`\n"
-                }
-            ]
-        });
-        return;
-    }
-    // Send empty HTTP 200 to original request
-    res.send('');
-
-    search_terms = convertText(text).join('+');
-    if(search_terms === ''){
-        axios.post(response_url, {
-            "Content-type" : "application/json",
-            "response_type": "ephemeral",
-            "text": '*Critical fail!* Check your spelling and try asking again.',
-            "attachments": [
-                {
-                    "text": 'Make sure you type the name of the spell.'
-                }
-            ]
-        });
-        return;
-    }
-    axios.get('http://www.dnd5eapi.co/api/spells/?name=' + search_terms)
-    .then(res => {
-        const spell_url = res.data.results[0].url;
-        axios.get(spell_url)
+    helpers.verify(req, res)
         .then(res => {
-            const spell = `*Spell:* ${res.data.name}\n*Description:* ${res.data.desc.join(' ')}\n*Range:* ${res.data.range}\n*Duration:* ${res.data.duration}\n*Concentration:* ${res.data.concentration}\n*Casting Time:* ${res.data.casting_time}\n*Page:* ${res.data.page}\n`;
-            axios.post(response_url, {
-                "Content-type" : "application/json",
-                "response_type": "ephemeral",
-                "text": spell
-            });
+            const { command, text, response_url } = req.body;
+            if (text.toLowerCase() === 'help') {
+                res.send({
+                    "response_type": "ephemeral",
+                    "text": `Use \`/cast\` to find out about 5th Edition Dungeons and Dragons spells. Some examples include:`,
+                    "attachments": [
+                        {
+                            "text": "• \`/cast fireball\`\n• \`/cast acid arrow\`\n• \`/cast animate dead\`\n"
+                        }
+                    ]
+                });
+                return;
+            }
+            // Send empty HTTP 200 to original request
+            res.send('');
+
+            search_terms = helpers.convertText(text).join('+');
+            if (search_terms === '') {
+                axios.post(response_url, {
+                    "Content-type": "application/json",
+                    "response_type": "ephemeral",
+                    "text": '*Critical fail!* Check your spelling and try asking again.',
+                    "attachments": [
+                        {
+                            "text": 'Make sure you type the name of the spell.'
+                        }
+                    ]
+                });
+                return;
+            }
+            axios.get('http://www.dnd5eapi.co/api/spells/?name=' + search_terms)
+                .then(res => {
+                    const spell_url = res.data.results[0].url;
+                    axios.get(spell_url)
+                        .then(res => {
+                            const spell = `*Spell:* ${res.data.name}\n*Description:* ${res.data.desc.join(' ')}\n*Range:* ${res.data.range}\n*Duration:* ${res.data.duration}\n*Concentration:* ${res.data.concentration}\n*Casting Time:* ${res.data.casting_time}\n*Page:* ${res.data.page}\n`;
+                            axios.post(response_url, {
+                                "Content-type": "application/json",
+                                "response_type": "ephemeral",
+                                "text": spell
+                            });
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        })
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                    axios.post(response_url, {
+                        "Content-type": "application/json",
+                        "response_type": "ephemeral",
+                        "text": '*Critical fail!* Check your spelling and try asking again.',
+                        "attachments": [
+                            {
+                                "text": `You searched for '${text}'.`
+                            }
+                        ]
+                    });
+                })
         })
-        .catch(function (error) {
-            console.log(error);
+        .catch(res => {
+            console.log('Unknown request');
         })
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-        axios.post(response_url, {
-            "Content-type" : "application/json",
-            "response_type": "ephemeral",
-            "text": '*Critical fail!* Check your spelling and try asking again.',
-            "attachments": [
-                {
-                    "text":`You searched for '${text}'.`
-                }
-            ]
-        });
-    })
 })
 
 app.post('/feat', function (req, res) {
-    const { command, text, response_url } = req.body;
-    if(text.toLowerCase() === 'help'){
-        res.send({
-            "response_type": "ephemeral",
-            "text": `Use \`/feat\` to find out about 5th Edition Dungeons and Dragons features. Some examples include:`,
-            "attachments": [
-                {
-                    "text":"• \`/feat rage\`\n• \`/feat danger sense\`\n• \`/feat intimidating presence\`\n"
-                }
-            ]
-        });
-        return;
-    }
-    // Send empty HTTP 200 to original request
-    res.send('');
-    
-    search_terms = convertText(text).join(' ');
-    if(search_terms === ''){
-        axios.post(response_url, {
-            "Content-type" : "application/json",
-            "response_type": "ephemeral",
-            "text": '*Critical fail!* Check your spelling and try asking again.',
-            "attachments": [
-                {
-                    "text": 'Make sure you type the name of the feature.'
-                }
-            ]
-        });
-        return;
-    }
-    axios.get('http://www.dnd5eapi.co/api/features')
-    .then(res => {
-        feats = res.data.results;
-        for(var i = 0; i < feats.length; i++){
-            if(feats[i]['name'] === search_terms){
-                var url = feats[i]['url'];
-            }
-        }
-        if(url === undefined){
-            axios.post(response_url, {
-                "Content-type" : "application/json",
-                "response_type": "ephemeral",
-                "text": '*Critical fail!* Check your spelling and try asking again.',
-                "attachments": [
-                    {
-                        "text": `You searched for '${search_terms}'.`
-                    }
-                ]
-            });
-            return
-        }
-        axios.get(url)
+    helpers.verify(req, res)
         .then(res => {
-            const feat = `*Feat:* ${res.data.name}\n*Description:* ${res.data.desc.join(' ')}\n`;
-            axios.post(response_url, {
-                "Content-type" : "application/json",
-                "response_type": "ephemeral",
-                "text": feat
-            });
+            const { command, text, response_url } = req.body;
+            if (text.toLowerCase() === 'help') {
+                res.send({
+                    "response_type": "ephemeral",
+                    "text": `Use \`/feat\` to find out about 5th Edition Dungeons and Dragons features. Some examples include:`,
+                    "attachments": [
+                        {
+                            "text": "• \`/feat rage\`\n• \`/feat danger sense\`\n• \`/feat intimidating presence\`\n"
+                        }
+                    ]
+                });
+                return;
+            }
+            // Send empty HTTP 200 to original request
+            res.send('');
+
+            search_terms = helpers.convertText(text).join(' ');
+            if (search_terms === '') {
+                axios.post(response_url, {
+                    "Content-type": "application/json",
+                    "response_type": "ephemeral",
+                    "text": '*Critical fail!* Check your spelling and try asking again.',
+                    "attachments": [
+                        {
+                            "text": 'Make sure you type the name of the feature.'
+                        }
+                    ]
+                });
+                return;
+            }
+            axios.get('http://www.dnd5eapi.co/api/features')
+                .then(res => {
+                    feats = res.data.results;
+                    for (var i = 0; i < feats.length; i++) {
+                        if (feats[i]['name'] === search_terms) {
+                            var url = feats[i]['url'];
+                        }
+                    }
+                    if (url === undefined) {
+                        axios.post(response_url, {
+                            "Content-type": "application/json",
+                            "response_type": "ephemeral",
+                            "text": '*Critical fail!* Check your spelling and try asking again.',
+                            "attachments": [
+                                {
+                                    "text": `You searched for '${search_terms}'.`
+                                }
+                            ]
+                        });
+                        return
+                    }
+                    axios.get(url)
+                        .then(res => {
+                            const feat = `*Feat:* ${res.data.name}\n*Description:* ${res.data.desc.join(' ')}\n`;
+                            axios.post(response_url, {
+                                "Content-type": "application/json",
+                                "response_type": "ephemeral",
+                                "text": feat
+                            });
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        })
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
         })
-        .catch(function (error) {
-            console.log(error);
+        .catch(res => {
+            console.log('Unknown request');
         })
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
 })
-
-
-function convertText(text){
-    var terms = text.split(" ");
-    for(var i = 0 ; i < terms.length ; i++){
-        terms[i] = terms[i].charAt(0).toUpperCase() + terms[i].substring(1); 
-    }
-    return terms;
-}
