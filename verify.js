71bfd2d80e8327a988c2717ab02a898998d98685
Noah Boulton <noah.boulton@gmail.com>
2019-06-04 11:08:50 -0700

    Working on verify now using qs

--- verify.js
+++ verify.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const qs = require(‘qs’);
+const qs = require('qs');
 
 function verify(request){
     const slack_signature = request.headers['x-slack-signature'];
